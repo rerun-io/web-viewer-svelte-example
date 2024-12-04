9f627216d7182d5d4b47c17117132d842d174579
jprochazk <pjanv42@gmail.com>
2024-12-04 13:32:31 +0100

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
-export default defineConfig({
+const config: UserConfig = {
   plugins: [wasm(), sveltekit()],
   // https://github.com/rerun-io/rerun/issues/6815
   optimizeDeps: {
     exclude: process.env.NODE_ENV === "production" ? [] : ["@rerun-io/web-viewer"],
   },
-});
+};
+
+if ("REPOSITORY" in process.env) {
+  config.base = `/${process.env.REPOSITORY}/`;
+}
+
+export default config;
 
